6a44c1d0f53dbbb4cf177a9ffb6d679e452695a8
Apps servisalud <ldavidme7@gmail.com>
2022-07-21 16:55:58 -0500

    Creación del directorio

--- Código.js
+++
@@ -1,8 +0,0 @@
-function doGet(){
-    let template = HtmlService.createTemplateFromFile('src/views/index.html').evaluate()
-    return template
-}
-
-function include(filename) {
-    return HtmlService.createHtmlOutputFromFile(filename).getContent();
-  }
